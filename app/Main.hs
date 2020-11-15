d7f561855b998104d1702c389336dfb894b43478
Josh <ahimaz.joshua@gmail.com>
2020-11-15 19:44:36 +0800

    Created Menu Prompt

--- app/Main.hs
+++ app/Main.hs
@@ -4,3 +4,19 @@ import Lib
 
 main :: IO ()
 main = someFunc
+
+-- Menu Code Prompt
+menu = do
+    putStrLn "╔════════════════════════════════════════════════════════════════════════════╗"
+    putStrLn "║                       >>> BMI Calculator App <<<                           ║"
+    putStrLn "║                                                                            ║"
+    putStrLn "║ [1] Calculate BMI                                                          ║"
+    putStrLn "║ [2] View Previous Readings                                                 ║"
+    putStrLn "║                                                                            ║"
+    putStrLn "║ [3] Exit                                                                   ║"
+    putStrLn "║                                                                            ║"
+    putStrLn "║ Please select one of the following choices ~~~                             ║"
+    putStrLn "╚════════════════════════════════════════════════════════════════════════════╝"
+    getLine
+
+-- doMenuRecursion = menu
