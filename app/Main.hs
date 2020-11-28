3afa2ba07a773b7bf2c1829536b234d45c3ca55f
Josh <ahimaz.joshua@gmail.com>
2020-11-28 18:25:34 +0800

    Setup UTIL structure

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,15 @@
 module Main where
 
 import Lib
+import Utils.IOValidation
 
 main :: IO ()
-main = someFunc
+main = menuRecursion
 
 -- Menu Code Prompt
 menu = do
     putStrLn "╔════════════════════════════════════════════════════════════════════════════╗"
-    putStrLn "║                       >>> BMI Calculator App <<<                           ║"
+    putStrLn "║                        >>> BMI Calculator App <<<                          ║"
     putStrLn "║                                                                            ║"
     putStrLn "║ [1] Calculate BMI                                                          ║"
     putStrLn "║ [2] View Previous Readings                                                 ║"
@@ -19,4 +20,19 @@ menu = do
     putStrLn "╚════════════════════════════════════════════════════════════════════════════╝"
     getLine
 
--- doMenuRecursion = menu
+-- Choices for Menu
+menuSelection :: String -> IO ()
+menuSelection "1" = putStrLn "Choice 1"
+menuSelection "2" = test
+menuSelection "3" = exitMenu
+
+
+-- Recursive menu
+menuRecursion :: IO ()
+menuRecursion = menu >>= menuSelection
+
+-- Exit Menu
+exitMenu :: IO ()
+exitMenu = putStrLn "See ya!"
+
+
---
+++ app/Utils/IOValidation.hs
@@ -0,0 +1,18 @@
+module Utils.IOValidation where
+
+test :: IO()
+test = putStrLn "Testing"
+
+{-
+
+    Input/Output Validation Utility
+
+    Created by Joshua Daveston Ahimaz
+    16102543
+    
+    for the purpose of ---
+    
+    Functional Programming Principles - PRG2214
+    Final Project
+
+-}
